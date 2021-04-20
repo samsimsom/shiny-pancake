9dbeba4230ae540913dc2dca1b429c2652138c45
samsimsom <samsimsom@gmail.com>
2021-04-20 19:38:52 +0300

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 
-
+import os
 from flask import Flask
+from config import DevelopmentConfig
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(DevelopmentConfig)
 
     # Blueprint Registration
     from app.main import main
---
+++ config.py
@@ -0,0 +1,48 @@
+import os
+from datetime import timedelta
+from dotenv import load_dotenv
+
+app_base_dir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(app_base_dir, '.env'))
+
+
+class Config(object):
+    # Flask
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    # CSRF
+    CSRF_ENABLED = True
+    WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_SECRET_KEY')
+
+    # Mongoengine
+    MONGODB_DB = os.environ.get('MONGODB_DB')
+    MONGODB_HOST = os.environ.get('MONGODB_HOST')
+    MONGODB_PORT = int(os.environ.get('MONGODB_PORT'))
+    MONGODB_USERNAME = os.environ.get('MONGODB_USERNAME')
+    MONGODB_PASSWORD = os.environ.get('MONGODB_PASSWORD')
+
+    # Upload
+    MAX_CONTENT_LENGTH = 4 * 1024 * 1024
+    UPLOAD_EXTENSIONS = ['.jpg', '.png', '.gif']
+    UPLOAD_PATH = os.path.join(app_base_dir, 'app/static/upload')
+
+    # Session
+    SESSION_COOKIE_NAME = 'SNID'
+    # SESSION_COOKIE_SECURE = True
+    # SESSION_COOKIE_HTTPONLY = True
+    # SESSION_COOKIE_SAMESITE = 'Lax'
+    PERMANENT_SESSION_LIFETIME = timedelta(days=7)
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
